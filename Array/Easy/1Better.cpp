b3291c92803643918f084e7187ec1070bead1156
akshita1204 <akshitatyagi04@gmail.com>
2025-04-27 17:01:24 +0530

    Initial commit

---
+++ Array/Easy/1Better.cpp
@@ -0,0 +1,21 @@
+/*
+class Solution {
+    public:
+        int largestElement(vector<int>& nums) {
+          int maxi=INT_MIN;
+          for(int ele:nums)
+          {
+            maxi=max(maxi,ele);
+          }
+          return maxi;
+        }
+    };
+
+*/
+
+//https://takeuforward.org/plus/dsa/problems/largest-element
+
+/*
+Time=> O(n)
+Space=> O(1)
+*/
