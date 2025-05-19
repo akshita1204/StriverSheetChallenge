9b9d6ed665c3e8ed8414d3cce2838ac95c30aee3
akshita1204 <akshitatyagi04@gmail.com>
2025-05-19 19:01:43 +0530

    Rearrange Array Elements by Sign

---
+++ Array/Medium/7.cpp
@@ -0,0 +1,30 @@
+
+
+//https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+/*
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+      int n=nums.size();
+      vector<int>ans(n);
+      int i=0; //pos
+      int j=1; //neg
+      for(int k=0;k<n;k++)
+      {
+          if(nums[k]>0)
+          {
+              ans[i]=nums[k];
+              i+=2;
+          }
+          else
+          {
+              ans[j]=nums[k];
+              j+=2;
+          }
+      }
+      return ans;
+
+    }
+};
+*/
